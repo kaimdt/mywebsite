1b74533da6f7c731292b2ed4819e695a191b18ff
Kai Lukas Marquardt <52718820+kaimdt@users.noreply.github.com>
2021-05-30 22:59:03 +0200

    Change Projects

--- pages/projects.js
+++ pages/projects.js
@@ -56,22 +56,33 @@ export default function Projects() {
             <img className={projects.brandmark} src="/assets/images/Leryon Brandmark Light.png" />
             <div className={projects.textblock}>
             <h2>Leryon</h2>
-            <p className={projects.description}>Leryon is a different kind of social media network. It offers you the best opportunity to create a suitable place for your community and yourself. Frontend: React Backend: NodeJS and Python</p>
+            <p className={projects.description}>Leryon is a different kind of social media network. It offers you the best opportunity to create a suitable place for your community and yourself.</p>
             </div>
+            <div className={projects.language}><a data-tooltip="NodeJS"><i class="fab fa-node-js"></i></a><a data-tooltip="Python"><i class="fab fa-python"></i></a><a data-tooltip="React"><i class="fab fa-react"></i></a></div>
           </a>
-          <a className={projects.project}>
-            <p className={projects.brandmark}>KAIMDT</p>
+          <a href="https://github.com/kaimdt/mywebsite" className={projects.project}>
+          <img className={projects.brandmark} src="/assets/images/kaimdt_brandmark.png" />
             <div className={projects.textblock}>
             <h2>kaimdt.com</h2>
             <p className={projects.description}>You are on this page. It was made with love.</p>
             </div>
+            <div className={projects.language}><a data-tooltip="NodeJS"><i class="fab fa-node-js"></i></a><a data-tooltip="React"><i class="fab fa-react"></i></a></div>
+          </a>
+          <a href="https://github.com/kaimdt/leryon-with-php" className={projects.project}>
+            <p className={projects.brandmark}>LAP</p>
+            <div className={projects.textblock}>
+            <h2>Leryon with PHP aka. LAP (Leryon Application with PHP)</h2>
+            <p className={projects.description}>Leryon was still developed with PHP until the end of 2020. Since the beginning of 2020 we are rewriting the entire source code in NodeJS and Python. Since we are no longer developing Leryon in PHP, we have decided to develop a Leryon PHP version, as we want to continue to push PHP.</p>
+            </div>
+            <div className={projects.language}><a data-tooltip="PHP"><i class="fab fa-php"></i></a></div>
           </a>
           <a className={projects.project}>
-            <p className={projects.brandmark}>CLIME</p>
+            <p className={projects.brandmark}>SH-Music</p>
             <div className={projects.textblock}>
-            <h2>Clime</h2>
-            <p className={projects.description}>Server Hosting.</p>
+            <h2>Share Music</h2>
+            <p className={projects.description}>With Share Music you can share your phone audio to other phones to listen to the same music everywhere at the next party.</p>
             </div>
+            <div className={projects.language}><a data-tooltip="Java"><i class="fab fa-java"></i></a><a data-tooltip="Python"><i class="fab fa-python"></i></a><a data-tooltip="Swift"><i class="fab fa-swift"></i></a></div>
           </a>
         </div>
       </main>
