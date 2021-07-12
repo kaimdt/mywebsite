3066d7cfec9c5b88c66d7471eec832d2213d116f
Kai Lukas Marquardt <52718820+kaimdt@users.noreply.github.com>
2021-07-12 14:23:10 +0200

    big update 1.0 => 2.0

--- pages/discord.js
+++ pages/discord.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link'
 import Navbar from '../components/Navbar';
 import NowPlaying from '../components/NowPlaying';
 import { motion } from 'framer-motion';
